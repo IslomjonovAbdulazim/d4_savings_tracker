498e912747fde9054307a26b7c4809193b8158d9
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-19 18:24:18 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,6 +17,8 @@ class _HomePageState extends State<HomePage> {
   int uzsTotal = 0;
   int usdTotal = 0;
 
+  final key = GlobalKey<FormState>();
+  final focus = FocusNode();
   final controller = TextEditingController();
   String currentCurrency = "UZS";
 
@@ -39,78 +41,103 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20),
-          child: Column(
-            children: [
-              // Status
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "${NumberFormat.decimalPattern().format(total)} UZS",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w700,
+          child: Form(
+            key: key,
+            child: Column(
+              children: [
+                // Status
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${NumberFormat.decimalPattern().format(total)} UZS",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "${NumberFormat.decimalPattern().format(usdTotal)} USD",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "${NumberFormat.decimalPattern().format(usdTotal)} USD",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
                     ),
-                  ),
-                  Text(
-                    "  ●  ",
-                    style: TextStyle(fontSize: 12, color: Colors.grey),
-                  ),
-                  Text(
-                    "${NumberFormat.decimalPattern().format(uzsTotal)} UZS",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
+                    Text(
+                      "  ●  ",
+                      style: TextStyle(fontSize: 12, color: Colors.grey),
                     ),
-                  ),
-                ],
-              ),
+                    Text(
+                      "${NumberFormat.decimalPattern().format(uzsTotal)} UZS",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
 
-              // History
-              SizedBox(height: 20),
-              Expanded(
-                child: SizedBox(),
-              ),
-              SizedBox(height: 20),
+                // History
+                SizedBox(height: 20),
+                Expanded(
+                  child: SizedBox(),
+                ),
+                SizedBox(height: 20),
 
-              // New Sum
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(),
-                  ),
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      if (currentCurrency == "UZS") {
-                        currentCurrency = "USD";
-                      } else if (currentCurrency == "USD") {
-                        currentCurrency = "UZS";
-                      }
-                      setState(() {});
-                    },
-                    child: Text(currentCurrency),
-                  ),
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {},
-                    child: Icon(Icons.send),
-                  ),
-                ],
-              ),
-            ],
+                // New Sum
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: controller,
+                        focusNode: focus,
+                        keyboardType: TextInputType.number,
+                        onTapOutside: (value) => focus.unfocus(),
+                        onChanged: (value) => key.currentState!.validate(),
+                        validator: (value) {
+                          int? sum = int.tryParse(value ?? "");
+                          if (value?.isEmpty == true) {
+                            return null;
+                          }
+
+                          if (sum == null) {
+                            return "Noto'g'ri Formatdagi Summa";
+                          }
+
+                          if (currentCurrency == "UZS") {
+                            if ((sum >= 5000 && sum <= 1000000) == false) {
+                              return "Minimum 5ming som va maksimum 1mln so'm kiriting";
+                            }
+                          }
+                        },
+                      ),
+                    ),
+                    CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        if (currentCurrency == "UZS") {
+                          currentCurrency = "USD";
+                        } else if (currentCurrency == "USD") {
+                          currentCurrency = "UZS";
+                        }
+                        setState(() {});
+                      },
+                      child: Text(currentCurrency),
+                    ),
+                    CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      child: Icon(Icons.send),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
